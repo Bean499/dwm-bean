fea16a8c9f83728d790f4a98cb43b68c31c1dd4e
Bean Robinson <bentr99@gmail.com>
2022-07-12 22:56:37 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env bash
-git submodule update --init --recursive
-mkdir -p out
-cp -r dwm out
+#!/bin/bash
+shopt -s dotglob
+# git submodule update --init --recursive
+rm -rf out
+mkdir out
+for file in dwm/*; do
+	cp $file out/
+done
 cp config.h out/
 cd out
-for patch in ../patches/; do
-	git apply "../patches/$patch"
+for patch in ../patches/*; do
+	git apply $patch;
 done
 make
 sudo make install
