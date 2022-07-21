ec871d72d1b6a12f12914d20df5f37047e18edc1
Bean Robinson <bentr99@gmail.com>
2022-07-21 19:53:29 +0100

    big config update

--- config.h
+++ config.h
@@ -8,24 +8,28 @@
 #include <X11/XF86keysym.h>
 
 /* gaps */
-static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#define FORCE_VSPLIT 0  /* nrowgrid layout: force two clients to always split vertically */
 /* gaps file must be imported after setting gaps */
 #include "vanitygaps.c"
 
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 2;        /* vertical padding for statusbar */
+static const char *fonts[]          = { "monospace:size=10",
+										"CozetteVector:size=10"};
+static const char dmenufont[] = "monospace:size=10";
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII" };
+static const char *tags[] = { "I", "II", "III", "IV", "V" };
 /* static const char *tags[] = { "home", "work", "code", "game", "misc", "musi", "disc" }; */
 
 static const Rule rules[] = {
@@ -34,13 +38,14 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Spotify",  		NULL,       NULL,       1 << 5,       0,           0  },
-	{ "discord",  		NULL,       NULL,       1 << 6,       0,           0  },
+	{ "Spotify",  		NULL,       NULL,       1 << 3,       0,           0  },
+	{ "discord",  		NULL,       NULL,       1 << 4,       0,           0  },
 	{ "qutebrowser",  	NULL,       NULL,       1 << 0,       0,           1  },
 	{ "mpv",		  	NULL,       NULL,       0,	 	      1,           -1 },
 	{ "feh",		  	NULL,       NULL,       0,	 	      1,           -1 },
 	{ "Lutris",		  	NULL,       NULL,       0,	 	      1,           -1 },
 	{ "Steam",		  	NULL,       NULL,       0,	 	      1,           -1 },
+	{ NULL,			  	"floaterm", NULL,       0,	 	      1,           -1 },
 };
 
 /* layout(s) */
@@ -52,17 +57,17 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[T]",      tile },    /* first entry is default */
-	{ "[M]",      monocle },
+	/* { "[M]",      monocle }, */
 	/* { "[@]",      spiral }, */
 	/* { "[\\]",     dwindle }, */
-	/* { "D[]",      deck }, */
+	/* { "[D]",      deck }, */
 	/* { "TTT",      bstack }, */
 	/* { "===",      bstackhoriz }, */
 	/* { "HHH",      grid }, */
 	/* { "###",      nrowgrid }, */
 	/* { "---",      horizgrid }, */
 	/* { ":::",      gaplessgrid }, */
-	/* { "|M|",      centeredmaster }, */
+	{ "[C]",      centeredmaster },
 	/* { ">M>",      centeredfloatingmaster }, */
 	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
@@ -84,11 +89,15 @@ static const Layout layouts[] = {
 /* dmenu commands - required by source, would love to remove since i don't use dmenu :[
 maybe one day i will muster the effort to change the source code so i can remove these */
 static char dmenumon[2] = "0"; /* component of dmenucmd in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *winmenu[] = { "rofi", "-show", "window", NULL };
+static const char *dmenucmd[] = { "/home/bean/.scripts/misc/dmenu.sh", "-m", dmenumon, NULL };
+/* static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL }; */
+/* static const char *winmenu[] = { "rofi", "-show", "window", NULL }; */
 
 /* terminal */
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "kitty", "-T", "term", NULL };
+/* static const char *termcmd[]  = { "tabbed", "-d", "-c", "-r", "2", "st", "-w", "''", NULL }; */
+/* static const char *termcmd[]  = { "st", NULL }; */
+/* static const char *floaterm[] = { "/home/bean/.scripts/misc/floaterm.sh", NULL }; */
 
 /* dashboard */
 static const char *dashboard[] = { "/home/bean/.config/eww/scripts/open_dashboard.sh", NULL };
@@ -119,87 +128,98 @@ static const char *next[]			= { "playerctl", "next", NULL };
 static const char *prev[]			= { "playerctl", "previous", NULL };
 static const char *print[]			= { "scrot", "-e", "mv $f ~/Screenshots/ 2>/dev/null & notify-send --icon=image-missing 'Screenshot taken.'", NULL };
 
+static const char *browser[]		= { "qutebrowser", NULL };
+static const char *editor[]			= { "kitty", "-e", "nvim", NULL };
+static const char *fileman[]		= { "kitty", "-e", "ranger", NULL };
+static const char *steam[]			= { "steam", NULL };
+static const char *discord[]		= { "discord", NULL };
+static const char *music[]			= { "spotify", NULL };
+static const char *pdf[]			= { "zathura", NULL };
+
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_d,      spawn,          {.v = dashboard } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = screenmenu } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = emojimenu } },
-	{ MODKEY,                       XK_c,      spawn,          {.v = clearnotifs } },
-	{ MODKEY,                       XK_x,      spawn,          {.v = clearwidgets } },
-	{ MODKEY|Mod1Mask,              XK_k,      spawn,          {.v = keyboardswap } },
-	{ MODKEY|Mod1Mask,              XK_j,      spawn,          {.v = musicmenu } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	/* modifier				key			function			argument */
+	{ MODKEY,				XK_space, 	spawn,			{.v = dmenucmd } },
+	{ MODKEY,				XK_Return,	spawn,			{.v = termcmd } },
+	/* { MODKEY|ControlMask,	XK_Return,	spawn,			{.v = floaterm } }, */
+	{ MODKEY,				XK_d,		spawn,			{.v = dashboard } },
+	{ MODKEY,				XK_p,		spawn,			{.v = screenmenu } },
+	{ MODKEY,				XK_e,		spawn,			{.v = emojimenu } },
+	{ MODKEY,				XK_c,		spawn,			{.v = clearnotifs } },
+	{ MODKEY,				XK_x,		spawn,			{.v = clearwidgets } },
+	{ MODKEY|Mod1Mask,		XK_k,		spawn,			{.v = keyboardswap } },
+	{ MODKEY|Mod1Mask,		XK_j,		spawn,			{.v = musicmenu } },
+	{ MODKEY,				XK_b,		togglebar,		{0} },
 
 	/* move focus */
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,				XK_j,		focusstack,		{.i = -1 } },
+	{ MODKEY,				XK_k,		focusstack,		{.i = +1 } },
 
 	/* grow master */
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,				XK_h,		setmfact,		{.f = -0.05} },
+	{ MODKEY,				XK_l,		setmfact,		{.f = +0.05} },
 
 	/* move window to master */
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	{ MODKEY|Mod1Mask,		XK_Return,	zoom,			{0} },
 
-	/* rofi window mode */
-	{ MODKEY,                       XK_Tab,    spawn,          { .v = winmenu } },
+	/* cycle tags */
+	{ MODKEY,				XK_Tab,		view,			{0} },
 
 	/* kill window */
-	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY,				XK_q,		killclient,     {0} },
 
 	/* change layouts */
-	{ MODKEY,                       XK_comma,      		setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_period,      	setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_slash,			setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,				XK_bracketleft,		cyclelayout,	{.i = -1 } },
+	{ MODKEY,				XK_bracketright,	cyclelayout,	{.i = +1 } },
+
+	{ MODKEY,				XK_comma,  			setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,				XK_period, 			setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,				XK_slash,			setlayout,      {.v = &layouts[2]} },
 
 	/* float window */
-	{ MODKEY|ControlMask,           XK_space,  togglefloating, {0} },
+	{ MODKEY|ControlMask,	XK_space,  togglefloating, {0} },
 
-	/* change tag */
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
+	/* volume keys */
+	{ NULL,					XF86XK_AudioRaiseVolume, 	spawn, {.v = upvol   } },
+	{ NULL,					XF86XK_AudioLowerVolume, 	spawn, {.v = downvol } },
+	{ NULL,					XF86XK_AudioMute, 			spawn, {.v = mutevol } },
 
-	/* Volume keys */
-	{ NULL,							XF86XK_AudioRaiseVolume, 	spawn, {.v = upvol   } },
-	{ NULL,							XF86XK_AudioLowerVolume, 	spawn, {.v = downvol } },
-	{ NULL,							XF86XK_AudioMute, 			spawn, {.v = mutevol } },
+	/* media keys */
+	{ NULL,					XF86XK_AudioPlay, 			spawn, {.v = playpause } },
+	{ NULL,					XF86XK_AudioNext, 			spawn, {.v = next } },
+	{ NULL,					XF86XK_AudioPrev, 			spawn, {.v = prev } },
 
-	/* Media keys */
-	{ NULL,							XF86XK_AudioPlay, 			spawn, {.v = playpause } },
-	{ NULL,							XF86XK_AudioNext, 			spawn, {.v = next } },
-	{ NULL,							XF86XK_AudioPrev, 			spawn, {.v = prev } },
+	/* print & colour picker */
+	{ NULL,					XK_Print, 	spawn, 		{.v = print } },
+	{ MODKEY|Mod1Mask,		XK_c,	 	spawn, 		{.v = colourpick } },
 
-	/* Print & colour picker */
-	{ NULL,							XK_Print, 	spawn, 		{.v = print } },
-	{ MODKEY|Mod1Mask,				XK_c,	 	spawn, 		{.v = colourpick } },
-
-	/* Swap focussed monitor */
-	{ MODKEY,                       XK_m,     	focusmon,	{.i = +1 } },
+	/* swap focussed monitor */
+	{ MODKEY,				XK_m,     	focusmon,	{.i = +1 } },
 
 	/* move window to other monitor */
-	{ MODKEY|ShiftMask,             XK_m,		tagmon,		{.i = +1 } },
+	{ MODKEY|ShiftMask,		XK_m,		tagmon,		{.i = +1 } },
 
 	/* center current floating window */
-	{ MODKEY,			           	XK_z,		movecenter,	{0} },
+	{ MODKEY,				XK_z,		movecenter,	{0} },
+
+	/* change tag */
+	TAGKEYS(				XK_1,                      0)
+	TAGKEYS(				XK_2,                      1)
+	TAGKEYS(				XK_3,                      2)
+	TAGKEYS(				XK_4,                      3)
+	TAGKEYS(				XK_5,                      4)
+
+	/* quit dwm */
+	{ MODKEY|ControlMask,	XK_q,		quit,		{0} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	/* { ClkLtSymbol,          0,              Button1,        setlayout,      {0} }, */
+	/* { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} }, */
+	{ ClkRootWin,			MODKEY,			Button1,		spawn,			{.v = termcmd} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
