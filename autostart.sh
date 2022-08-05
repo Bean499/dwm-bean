cef88d400dc503cf7d639623767019040fbb19a9
Bean Robinson <bentr99@gmail.com>
2022-08-05 13:54:41 +0100

    now starts spotify-tui, but only if there is more than one connected monitor

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+monitors=$(xrandr --query | grep " connected" | wc -l)
+
+dwmstatus &
 qutebrowser &
 spotify &
 discord &
-~/.scripts/notifications/update-notify.sh &
-dwmstatus
+
+if [ "$monitors" != "1" ]; then
+	kitty -T spotify-tui --override "font_size=16" -e spt &
+	~/.scripts/notifications/update-notify.sh &
+fi
