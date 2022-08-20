5af61f0e7a4b4978633f1732118d26ee153dab85
Bean Robinson <bentr99@gmail.com>
2022-08-20 19:55:47 +0100

    dixed autostart script

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 monitors=$(xrandr --query | grep " connected" | wc -l)
 
+~/.scripts/notifications/update-notify.sh &
 dwmstatus &
 qutebrowser &
-spotify &
+# spotify &
 discord &
 
 if [ "$monitors" != "1" ]; then
-	kitty -T spotify-tui --override "font_size=16" -e spt &
-	~/.scripts/notifications/update-notify.sh &
+	echo "Two monitors connected" &
+	# kitty -T spotify-tui --override "font_size=16" -e spt &
+	# kdeconnect-app &
 fi
